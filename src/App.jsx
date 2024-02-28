36cf2318423478d0a4647b8912787c840d10becb
Sequoia Nevae <sequoianevae@Sequoias-iMac.lan>
2024-02-27 20:39:09 -0600

    updated workshop

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,24 @@ import './App.css'
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
   // console.log(puppyList)
-  console.log("puppyList: ", puppyList);
+  // console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
   
+  // function handleClick () {
+  //   // some logic here
+  // }
+
   return (
-    <>
       <div className="App">
         {
           puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={()=>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+
+
         })
         }
         </div>
-    </>
-  )
+  );
 }
 
 export default App
